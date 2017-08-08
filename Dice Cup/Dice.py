a93fb68abc91475c080734e834afc9e72bdabc06
jb1361 <justinbutler4@hotmail.com>
2017-08-08 00:15:10 -0400

    problems not done

--- Dice Cup/Dice.py
+++ Dice Cup/Dice.py
@@ -1,5 +1,24 @@
 input = input()
 data = input.split()
+d1 = int(data[0])
+d2 = int(data[1])
 
-output = ((int(data[0])*int(data[1])) - (int(data[0])-1))
-print(str(output))
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
+
+mostcommon = []
+combinations = d1 * d2
+history = []
+i = 1
+j = 1
+while i <= d1:
+	j = 1
+	while j <= d2:
+		history.append(i+j)
+		j+=1
+	i+=1
+
+mostcommon = most_common(history)
+print(mostcommon)
+#need to return multiple ones that have the same probability
---
+++ Faktor/Faktor.py
@@ -0,0 +1,5 @@
+input = input()
+data = input.split()
+
+output = ((int(data[0])*int(data[1])) - (int(data[0])-1))
+print(str(output))
