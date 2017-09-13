718705ccd2cbcf30afbe2b5248b5a52feddf970a
jb1361 <justinbutler4@hotmail.com>
2017-09-12 20:36:29 -0400

    work

--- Honeycomb/Honey.py
+++ Honeycomb/Honey.py
@@ -6,31 +6,17 @@ i = 0
 while i < cases:
 	data.append(input())
 	i+=1
-	
-"""	
-def calculate_greater_than_three(n):
-	max_layer = math.floor(n/2)
-	moves_at_max = (6*(2**(max_layer-1)))*3
-	intermediate_moves = 5 *(6*)
-	return_moves = (6 * max_layer)
-	out = moves_at_max + return_moves
-	output.append(out)
-	
-def calculate(n):
-	max_layer = math.floor(n/2)
-	moves_at_max = (6*(2**(max_layer-1)))*3
-	return_moves = (6 * max_layer)
-	out = moves_at_max + return_moves
-	output.append(out)
-	"""
+"""
+Try adding combs * 3 from all previous layers
+or go back to just calculating tracks based on n with formulas
+"""
+
 def calculate(prev, i, n):
-	
 	if i < n:
 		if (i % 2)  == 0:
-			
-			max_layer = math.floor(i/2)
-			combs_at_next_layer = 6 * (2**max_layer)
-			next = (prev * 5) - combs_at_next_layer
+		#	max_layer = math.floor(i/2)
+			#combs_at_max_layer = 6 * ((2**max_layer)-1)
+			next = (prev * 3)
 			return calculate(next,i+1,n)
 		elif (i % 2) != 0:
 			
@@ -43,9 +29,9 @@ def calculate(prev, i, n):
 def compute(n):
 	if n == 1:
 		output.append(0)
-	elif n == 2:
+	if n == 2:
 		output.append(6)
-	elif n > 2:
+	else:
 		out = calculate(6,2,n)
 		output.append(out)
 def start():
